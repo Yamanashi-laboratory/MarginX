fc69511525db8492ee707eb5f1377e5f41bc6409
Sho-Matsuoka <syouc9@yahoo.co.jp>
2023-12-19 14:41:49 +0900

    12/19

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import subprocess
 input = input("Are you using ninniku (server)? (y / n)")
 
 if input.lower() == 'n':
+    print("Please input the command which executes JoSIM. ex:) josim_v265, josim-cli, josim etc...")
     result1 = subprocess.run(["make", "mew"], stdout=subprocess.PIPE, text=True)
     result1_exe = subprocess.run(["./mew"], stdout=subprocess.PIPE, text=True)
-    print("Please input the command which executes JoSIM. ex:) josim_v265, josim-cli, josim etc...")
 
 result2 = subprocess.run(["make", "setup"], stdout=subprocess.PIPE, text=True)
 result2_exe = subprocess.run(["./setup"], stdout=subprocess.PIPE, text=True)
